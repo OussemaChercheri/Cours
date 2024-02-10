ffd057238cf8335f705a100886f26396eb8bb7ad
Oussema <oussama.chercheri@tek-up.de>
2024-02-11 00:45:29 +0100

    learn crud api in express js

--- MEAN/crud-api/index.js
+++ MEAN/crud-api/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+require('express-async-errors');
 const db = require('./db'),
     routes = require('./controllers/employee.controller')
 
 //middleware
-app.use('/api/employees', routes)
+app.use(bodyparser.json())
+app.use('/api/employees', routes);
+
+app.use((err, req, res, next) =>{
+    console.log(err);
+    res.status(err.status || 500).send("Somthing went wrong!");
+});
 
 
 db.query("SELECT 1")
