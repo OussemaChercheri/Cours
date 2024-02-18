4a09106c9285999523ac6fb21e7936eb49fe1c18
Oussema <oussama.chercheri@tek-up.de>
2024-02-18 23:31:58 +0100

    JS cours

---
+++ JS/Website/script.js
@@ -0,0 +1,14 @@
+console.log(`Hello`);
+
+window.alert(`This an alert message`);
+
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `This is a paragraph`;
+
+//this a comment
+
+/*
+
+This is a multi-line comment
+ */
