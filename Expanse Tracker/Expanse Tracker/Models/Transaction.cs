a8ac9e5507b73900b88acfbc4c93f6655f69020d
Oussama CHERCHERI <oussama.chercheri@tek-up.de>
2024-05-04 20:55:57 +0100

    that it for today

---
+++ Expanse Tracker/Expanse Tracker/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expanse_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryId
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
