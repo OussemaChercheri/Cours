155badf51bb81c3104aadce80f2838a1186e67e7
Oussema Chercheri <oussama.chercheri@tek-up.de>
2024-05-27 21:43:09 +0100

    create a new module

--- angular/exam/src/app/app.module.ts
+++ angular/exam/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { ChildComponent } from './child/child.component';
-import { CvComponent } from './cv/cv.component';
-import { DetailsComponent } from './details/details.component';
-import { ItemComponent } from './item/item.component';
-import { ListeComponent } from './liste/liste.component';
+
 
 @NgModule({
 
-  declarations: [AppComponent, FirstComponent, ChildComponent, CvComponent, DetailsComponent, ItemComponent, ListeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
